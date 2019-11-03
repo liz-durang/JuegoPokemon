8e7b102f1e2d6ed4c23bf1c99cbdb504fcaa24d4
lizduran <42351387+lizduran@users.noreply.github.com>
2019-11-02 19:46:02 -0600

    Update Pokemon.java

--- Proyecto JAVA/Pokemon.java
+++ Proyecto JAVA/Pokemon.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
-import java.util.Iterator;
-
+import java.util.Scanner;
 public class Pokemon implements Cloneable{
 
-    public int PS; //puntos de salud  
-    public int ataque; 
-    public int defensa; 
-    public int numPokedex;
-    public int velocidad; 
-    public String tipo; 
-    public String nombre; 
+    private int PS; //puntos de salud  
+    private int ataque; 
+    private int defensa; 
+    private int numPokedex;
+    private int velocidad; 
+    private String tipo; 
+    private String nombre; 
 
+    Scanner entrada = new Scanner (System.in);  
     ArrayList <Ataque> movimientos = new ArrayList <Ataque> ();
     
 
@@ -47,7 +47,7 @@ public class Pokemon implements Cloneable{
     public int getAtaque () {return ataque;}
     public int getDefensa () {return defensa;}
 
-    public Pokemon ClonePoke (Pokemon pokeAClonar)
+    public Pokemon clonePoke (Pokemon pokeAClonar)
     {
         Pokemon copiaPoke = null;
         try {
@@ -60,13 +60,95 @@ public class Pokemon implements Cloneable{
         return copiaPoke;
     }
 
+    @Override
     public String toString()
     {
     
         return  nombre; 
     }
 
-    
+
+    public Ataque ElegirAtaque()
+    {
+        //imprimir los ataques del pokemon 
+        //regresa elnombre del ataque
+        Ataque ataqueElegido = new Ataque();
+        for (Ataque ataque : this.movimientos) 
+        {
+            System.out.print (ataque.getNombre() + "  ");
+        }
+        int bandera = 0; 
+        while (bandera == 0) 
+        {
+            System.out.print("\n---> ");
+            String name = entrada.nextLine ();
+            //se seleccionara con el que se jugara.
+            for (Ataque ataque : this.movimientos) 
+            {
+                if (ataque.getNombre().equals(name))
+                {
+                    ataqueElegido = ataque; 
+                    bandera = 1;
+                    break;
+                }
+            }
+        }
+        return ataqueElegido; 
+    }
+
+
+
+    public double hacerDaño (Pokemon pokeDefensa, Ataque ataqueUsado)
+    {
+        //Bonificacion
+        double B = 0; 
+        if (ataqueUsado.getTipo().equals(this.tipo))
+        {
+            B = 1.5;
+        }
+        else{
+            B = 1; 
+        }
+
+        System.out.println("\n B es: "+B);
+
+        //Efectividad 
+        double E  ;
+        int random = ((int)(Math.random()*6+1) ) ; 
+        if (random == 0) {E = 0;} 
+        else if (random == 1){E = 0.25;}
+        else if (random == 2){E = 0.5;}
+        else if (random == 3) { E = 1;}
+        else if (random == 4){E = 2;}
+        else{E = 4;}
+
+        System.out.println("\n E es: "+E);
+
+        //Variacion //random()*cantidad de numeros en el rango + termino inicial del rango
+        double V = ((int)(Math.random()*15+85)); 
+
+        System.out.println("\n V es: "+V);
+
+        //Cantidad Ataque
+        double A = this.ataque;
+
+        System.out.println("\n A es: "+A);
+
+        //Potencial del ataque 
+        double P = ataqueUsado.getPotencia();
+
+        System.out.println("\n P es: "+P);
+         
+        //Defensa
+        double D = pokeDefensa.defensa;
+
+        System.out.println("\nD es: "+D);
+
+
+        double daño = (0.01*B*E*V)*(    (((0.2*1)+1)*A*P)    / (25*D)  +2);
+
+        return daño; 
+    }
     
 
 }
